f079369a2227d8c5ef12ab182f9f0ddc26519019
Jamison Griffith <jamison.griffith@gmail.com>
2021-04-20 13:09:08 -0600

    Add item class with test

---
+++ spec/item_spec.rb
@@ -0,0 +1,15 @@
+require './lib/item'
+
+RSpec.describe Item do
+
+  describe 'initialize' do
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: "$2.50"})
+
+    it 'exists and has accessible attributes' do
+      expect(item1).to be_instance_of(Item)
+      expect(item1.name).to eq('Peach Pie (Slice)')
+      expect(item1.price).to eq('$3.75')
+    end
+  end
+end
