574046f28b358b3ad535d144eac3c78e3d1c56fd
Jamison Griffith <jamison.griffith@gmail.com>
2021-04-20 14:03:41 -0600

    Implement food_trucks_that_sell

--- lib/event.rb
+++ lib/event.rb
@@ -6,4 +6,37 @@ class Event
     @food_trucks = []
   end
 
+  def add_food_truck(truck)
+    @food_trucks << truck
+  end
+
+  def food_truck_names
+    @food_trucks.map { |truck| truck.name }
+  end
+
+  def food_trucks_that_sell(item_name)
+    found_trucks = []
+    item_object = get_item_from(item_name)[0]
+    # require 'pry';binding.pry
+    @food_trucks.each do |truck|
+      if truck.check_stock(item_object) > 0
+        found_trucks << truck
+      end
+    end
+    found_trucks.uniq
+  end
+
+  def get_item_from(item_name)
+    @food_trucks.each do |truck|
+      truck.inventory.each do |item|
+        # require 'pry'; binding.pry
+        if item[0].name == item_name
+          return item
+        else
+          nil
+        end
+      end
+    end
+  end
+
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -7,14 +7,37 @@ RSpec.describe Event do
   describe 'initialization' do
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     item2 = Item.new({name: 'Apple Pie (Slice)', price: "$2.50"})
-    food_truck = FoodTruck.new("Rocky Mountain Pies")
-    food_truck.stock(item1, 25)
-    food_truck.stock(item2, 12)
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2.stock(item3, 25)
+    food_truck2.stock(item4, 50)
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3.stock(item1, 65)
     event = Event.new("South Pearl Street Farmers Market")
 
     it 'exists and accesses it attributes' do
       expect(event.name).to eq("South Pearl Street Farmers Market")
       expect(event.food_trucks).to be_instance_of(Array)
+    end
+
+    it 'can add food trucks and return names' do
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+      expect(event.food_trucks.length).to eq(3)
+      expect(event.food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+    end
+
+    it 'can return list of food trucks from item name' do
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+      name = "Banana Nice Cream"
+      expect(event.food_trucks_that_sell(name)).to eq([food_truck2])
     end
 
   end
